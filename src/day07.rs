c482507bade1dec74c2c2bb3d830c509db26078b
Adam Basile <adam.basile@veitchlister.com>
2022-12-07 17:56:13 +1100

    Data types

--- src/day07.rs
+++ src/day07.rs
@@ -1,4 +1,32 @@
-pub(crate) fn day07(lines: Vec<String>) -> (i32, i32) {
+use std::collections::HashMap;
+use std::fmt;
+
+// create a tree using the arena pattern to resolve ownership
+
+
+#[derive(Debug)]
+struct File {
+    name: String,
+    size: i32,
+}
+
+#[derive(Debug)]
+struct Directory {
+    name: String,
+    parent: usize,
+    files: Vec<usize>,
+    directories: HashMap<str, usize>,
+}
+
+#[derive(Debug)]
+struct FileSystem {
+    root: Directory,
+    files: Vec<File>,
+    directories: Vec<Directory>,
+    cwd: usize,
+}
+
+pub fn day07(lines: Vec<String>) -> (i32, i32) {
     (0, 0)
 }
 
